e7f853c7980ebcaa4ad933af721e1f55dbc6dffe
vishnuur <vishnuur77@gmail.com>
2024-05-26 21:21:12 +0530

    removed unused imports

--- src/screens/Users/Profile/Components/viewAddPhotos.tsx
+++ src/screens/Users/Profile/Components/viewAddPhotos.tsx
@@ -1,5 +1,5 @@
 import { UploadOutlined } from "@ant-design/icons";
-import { Button, Dropdown, Image, MenuProps, Modal, Upload } from "antd";
+import { Modal, Upload } from "antd";
 import { useRef, useState } from "react";
 import { MdEditSquare, MdZoomIn } from "react-icons/md";
 import { customToast } from "src/components/Toast";
@@ -68,45 +68,6 @@ export default function ViewAddPhotos({
     // Read the image file as Data URL
     reader.readAsDataURL(file);
   };
-
-  // const items: MenuProps["items"] = [
-  //   {
-  //     key: "1",
-  //     label: (
-  //       <a
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //         href="https://www.antgroup.com"
-  //       >
-  //         1st menu item
-  //       </a>
-  //     ),
-  //   },
-  //   {
-  //     key: "2",
-  //     label: (
-  //       <a
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //         href="https://www.aliyun.com"
-  //       >
-  //         2nd menu item
-  //       </a>
-  //     ),
-  //   },
-  //   {
-  //     key: "3",
-  //     label: (
-  //       <a
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //         href="https://www.luohanacademy.com"
-  //       >
-  //         3rd menu item
-  //       </a>
-  //     ),
-  //   },
-  // ];
 
   const onEditIconClicked = () => {
     (fileInputRef as any).current.click();
